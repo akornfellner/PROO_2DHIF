bbcb156b11edb3fb8ddf5c08654863657ff7a88e
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2022-12-13 10:25:58 +0100

    AoC day1 part2 added

---
+++ AdventOfCode/Day1P2/Program.cs
@@ -0,0 +1,23 @@
+﻿var data = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+""";
+
+var mealsOfElfs = data.Split("\n\n").Select(e => e.Split("\n").Select(int.Parse).Sum())
+    .OrderByDescending(n => n)
+    .Take(3)
+    .Sum();
+
+Console.WriteLine(mealsOfElfs);
